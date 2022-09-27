b18c5bd0b17b15ce5dad80b38df85352ce463fdf
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-27 16:02:37 +0700

    Buffered channel

--- channel.go
+++ channel.go
@@ -2,17 +2,27 @@ package main
 
 import "fmt"
 
-// Unbuffered  channel
-
 func main() {
 	//Unbuffered  channel
-	ch := make(chan int)
 
-	go func() {
-		ch <- 100
-	}()
+	// ch := make(chan int)
+
+	// go func() {
+	// 	ch <- 100
+	// }()
+
+	// fmt.Println(<-ch)
+
+	// fmt.Println("done.")
+
+	//Buffered channel
+	ch := make(chan int, 2)
+
+	ch <- 1
+	ch <- 2
+	close(ch)
 
 	fmt.Println(<-ch)
-
-	fmt.Println("done.")
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
